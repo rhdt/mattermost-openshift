ee8341ade9c9a31b7afc46985706dd93b77f579d
Jean-Francois Chevrette <jfchevrette@gmail.com>
2017-03-09 10:24:53 -0500

    add support for s3 config at runtime

--- mattermost-team/docker-entrypoint.sh
+++ mattermost-team/docker-entrypoint.sh
@@ -29,7 +29,24 @@ if [[ "$1" == "mattermost" ]]; then
   echo "Updating mattermost configuration..."
   updatejson ".SqlSettings.DriverName" "postgres" $MM_CONFIG
   updatejson ".SqlSettings.DataSource" "postgres://${MM_DB_USER}:${MM_DB_PASS}@${MM_DB_HOST}:${MM_DB_PORT}/${MM_DB_NAME}?sslmode=disable&connect_timeout=10" $MM_CONFIG
+
+  # Check if we want to use S3 for data storage
+  if [[ "$MM_USE_S3" == "true" ]]; then
+    if [[ -z $MM_S3_ACCESS_KEY_ID ]]; then echo "MM_S3_ACCESS_KEY_ID not set."; exit 1; fi
+    if [[ -z $MM_S3_SECRET_ACCESS_KEY ]]; then echo "MM_S3_SECRET_ACCESS_KEY not set."; exit 1; fi
+    if [[ -z $MM_S3_BUCKET ]]; then echo "MM_S3_BUCKET not set."; exit 1; fi
+
+    MM_S3_REGION=${MM_S3_REGION:-us-east-1}
+    MM_S3_ENDPOINT=${MM_S3_ENDPOINT:-s3.amazonaws.com}
+
+    updatejson ".FileSettings.AmazonS3AccessKeyId" "${MM_S3_ACCESS_KEY_ID}" $MM_CONFIG
+    updatejson ".FileSettings.AmazonS3SecretAccessKey" "${MM_S3_SECRET_ACCESS_KEY}" $MM_CONFIG
+    updatejson ".FileSettings.AmazonS3Bucket" "${MM_S3_BUCKET}" $MM_CONFIG
+    updatejson ".FileSettings.AmazonS3Region" "${MM_S3_REGION}" $MM_CONFIG
+    updatejson ".FileSettings.AmazonS3Endpoint" "${MM_S3_ENDPOINT}" $MM_CONFIG
   
+  fi
+
   while ! echo | nc -w1 $MM_DB_HOST $MM_DB_PORT > /dev/null 2>&1; do
     echo "Could not connect to database at ${MM_DB_HOST}:${MM_DB_PORT}... Retrying..."
     sleep 1
